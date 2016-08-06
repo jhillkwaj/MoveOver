9c4b9642297354ececdcfa64af26dd209163fb37
Justin Hill <jhillkwaj@gmail.com>
2016-08-06 14:51:13 -0500

    Added firebase support for anonymous login and getting locations from the database using geofire

--- Client App/MoveOverClient/app/src/main/java/com/rahulyesantharao/www/moveoverclient/MainActivity.java
+++ Client App/MoveOverClient/app/src/main/java/com/rahulyesantharao/www/moveoverclient/MainActivity.java
@@ -3,6 +3,7 @@ package com.rahulyesantharao.www.moveoverclient;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Vibrator;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,19 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity implements Alert.OnFragmentInteractionListener {
 
     private String TAG_NOTHING = "nothing";
@@ -18,6 +32,12 @@ public class MainActivity extends AppCompatActivity implements Alert.OnFragmentI
     private NothingNearby nFrag = null;
     private boolean poweredOn = false;
     private MediaPlayer mediaPlayer = null;
+
+    private static boolean login = false;
+    FirebaseAuth mAuth;
+    String uid;
+    float distance = .6f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +58,33 @@ public class MainActivity extends AppCompatActivity implements Alert.OnFragmentI
         getSupportFragmentManager().beginTransaction().setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out).hide(nFrag).commit();
 
 //        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    login = true;
+                    // User is signed in
+                    Log.d("Login", "onAuthStateChanged:signed_in:" + user.getUid());
+
+                    uid = user.getUid();
+                    addLocListener(0,.0001f);
+                } else {
+                    login = false;
+                    // User is signed out
+                    Log.d("Login", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        mAuth.addAuthStateListener(mAuthListener);
+
+        if(!login)
+            signInAnonymously();
     }
 
     @Override
@@ -45,6 +92,44 @@ public class MainActivity extends AppCompatActivity implements Alert.OnFragmentI
         getMenuInflater().inflate(R.menu.menu, menu);
 
         return(super.onCreateOptionsMenu(menu));
+    }
+
+    //call this method with the latest user location
+    private void addLocListener(float lat, float lon){
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("geo-loc");
+
+        GeoFire geoFire = new GeoFire(ref);
+
+        //geoFire.setLocation("test-loc", new GeoLocation(24.7853889, -122.4056973));
+        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(lat, lon), distance);
+        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+                System.out.println(String.format("Key %s entered the search area at [%f,%f]", key, location.latitude, location.longitude));
+
+                //This is where I think you will want your code
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+                System.out.println(String.format("Key %s is no longer in the search area", key));
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+                System.out.println(String.format("Key %s moved within the search area to [%f,%f]", key, location.latitude, location.longitude));
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                System.out.println("All initial data has been loaded and events have been fired!");
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+                System.err.println("There was an error with this query: " + error);
+            }
+        });
     }
 
     @Override
@@ -132,5 +217,30 @@ public class MainActivity extends AppCompatActivity implements Alert.OnFragmentI
         findViewById(R.id.stopAlertBtn).setClickable(false);
         findViewById(R.id.stopAlertBtn).setEnabled(false);
         Log.d(getClass().getSimpleName(), "Stop Pressed");
+    }
+
+
+    private void signInAnonymously() {
+        Log.d("Login Anon", "Start Anon Login");
+        // [START signin_anonymously]
+        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                Log.d("Login Anon", "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                // If sign in fails, display a message to the user. If sign in succeeds
+                // the auth state listener will be notified and logic to handle the
+                // signed in user can be handled in the listener.
+                if (!task.isSuccessful()) {
+                    Log.w("Login Anon", "signInAnonymously", task.getException());
+
+                }
+            }
+
+
+        });
+
+
+        // [END signin_anonymously]
     }
 }
