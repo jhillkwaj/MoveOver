f0e266c877403e42d6be73f872d03a4d436887c1
Richard Dong <Richard Dong>
2016-08-06 14:57:13 -0500

    Added get location function to program.

--- Sender/MyApplication/app/src/main/java/com/example/moveover/moveover/MainActivity.java
+++ Sender/MyApplication/app/src/main/java/com/example/moveover/moveover/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.moveover.moveover;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,12 +24,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private LocationManager locationManager = null;
+    private LocationListener locationListener = null;
+    private static final String TAG = "Debug";
     private ToggleButton toggleButton1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -27,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
         addListenerOnButton();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        setRequestedOrientation(ActivityInfo
+                .SCREEN_ORIENTATION_PORTRAIT);
+        locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
 
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -58,6 +84,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
     @Override
+    public void onClick(View v) {
+        locationListener = new MyLocationListener();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            Log.v("Exit", "Loop ");
+            onClick(v);
+        }
+        else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 5, locationListener);
+            Log.v("working","working");
+        }
+
+
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -78,4 +121,29 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {
+            //lattitude = loc.getLatitude()
+            //longitude = loc.getLongitude()
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onStatusChanged(String provider,
+                                    int status, Bundle extras) {
+            // TODO Auto-generated method stub
+        }
+    }
+
 }
