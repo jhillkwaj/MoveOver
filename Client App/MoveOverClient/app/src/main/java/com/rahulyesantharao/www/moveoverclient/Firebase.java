d292c8023bed8e8c2806a13e3257d9354b09e2f7
Justin Hill <jhillkwaj@gmail.com>
2016-08-06 19:45:02 -0500

    Added location code

---
+++ Client App/MoveOverClient/app/src/main/java/com/rahulyesantharao/www/moveoverclient/Firebase.java
@@ -0,0 +1,128 @@
+package com.rahulyesantharao.www.moveoverclient;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Debug;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by justi on 8/6/2016.
+ */
+public class Firebase {
+
+    private static boolean login = false;
+    FirebaseAuth mAuth;
+    String uid;
+    float distance = .2f;
+    MainActivity mainActivity = null;
+    GeoQuery geoQuery = null;
+
+    public Firebase(MainActivity mainActivity)
+    {
+        this.mainActivity = mainActivity;
+
+        // Firebase code
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    login = true;
+                    // User is signed in
+                    Log.d("Login", "onAuthStateChanged:signed_in:" + user.getUid());
+
+                    uid = user.getUid();
+
+                } else {
+                    login = false;
+                    // User is signed out
+                    Log.d("Login", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void signInAnonymously() {
+        Log.d("Login Anon", "Start Anon Login");
+        // [START signin_anonymously]
+        mAuth.signInAnonymously().addOnCompleteListener(mainActivity, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                Log.d("Login Anon", "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                // If sign in fails, display a message to the user. If sign in succeeds
+                // the auth state listener will be notified and logic to handle the
+                // signed in user can be handled in the listener.
+                if (!task.isSuccessful()) {
+                    Log.w("Login Anon", "signInAnonymously", task.getException());
+
+                }
+            }
+
+
+        });
+    }
+
+        //call this method with the latest user location
+    public void addLocListener(double lat, double lon){
+        if(lat == 0 && lon == 0)
+        { return; }
+
+        Log.d("Test",lat + " " + lon);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("geo-loc");
+
+        GeoFire geoFire = new GeoFire(ref);
+
+        if(geoQuery != null)
+            geoQuery.removeAllListeners();
+
+        //geoFire.setLocation("test-loc", new GeoLocation(24.7853889, -122.4056973));
+        geoQuery = geoFire.queryAtLocation(new GeoLocation(lat, lon), distance);
+        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+                System.out.println(String.format("Key %s entered the search area at [%f,%f]", key, location.latitude, location.longitude));
+                if(mainActivity.poweredOn) mainActivity.turnOnAlert(true, true, true);
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+                System.out.println(String.format("Key %s is no longer in the search area", key));
+                if(mainActivity.poweredOn) mainActivity.turnOffAlert();
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+                System.out.println(String.format("Key %s moved within the search area to [%f,%f]", key, location.latitude, location.longitude));
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                System.out.println("All initial data has been loaded and events have been fired!");
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+                System.err.println("There was an error with this query: " + error);
+            }
+        });
+    }
+}
--- Client App/MoveOverClient/app/src/main/java/com/rahulyesantharao/www/moveoverclient/MainActivity.java
+++ Client App/MoveOverClient/app/src/main/java/com/rahulyesantharao/www/moveoverclient/MainActivity.java
@@ -17,31 +17,17 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.firebase.geofire.GeoFire;
-import com.firebase.geofire.GeoLocation;
-import com.firebase.geofire.GeoQuery;
-import com.firebase.geofire.GeoQueryEventListener;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity implements Alert.OnFragmentInteractionListener {
 
     private String TAG_NOTHING = "nothing";
     private String TAG_ALERT = "alert";
     private NothingNearby nFrag = null;
-    private boolean poweredOn = false;
+    public boolean poweredOn = false;
     private MediaPlayer mediaPlayer = null;
 
-    private static boolean login = false;
-    FirebaseAuth mAuth;
-    String uid;
-    float distance = .6f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,33 +54,11 @@ public class MainActivity extends AppCompatActivity implements Alert.OnFragmentI
         s.setSpan(new TypefaceSpan("bigNoodle.ttf"), 0, s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(s);
-        // Firebase code
-        mAuth = FirebaseAuth.getInstance();
 
 
-        FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    login = true;
-                    // User is signed in
-                    Log.d("Login", "onAuthStateChanged:signed_in:" + user.getUid());
+        Firebase firebase = new Firebase(this);
+        LocationData data = new LocationData(this,firebase);
 
-                    uid = user.getUid();
-                    addLocListener(0,.0001f);
-                } else {
-                    login = false;
-                    // User is signed out
-                    Log.d("Login", "onAuthStateChanged:signed_out");
-                }
-                // ...
-            }
-        };
-
-        mAuth.addAuthStateListener(mAuthListener);
-
-        if(!login)
-            signInAnonymously();
     }
 
     @Override
@@ -104,43 +68,7 @@ public class MainActivity extends AppCompatActivity implements Alert.OnFragmentI
         return(super.onCreateOptionsMenu(menu));
     }
 
-    //call this method with the latest user location
-    private void addLocListener(float lat, float lon){
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("geo-loc");
 
-        GeoFire geoFire = new GeoFire(ref);
-
-        //geoFire.setLocation("test-loc", new GeoLocation(24.7853889, -122.4056973));
-        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(lat, lon), distance);
-        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
-            @Override
-            public void onKeyEntered(String key, GeoLocation location) {
-                System.out.println(String.format("Key %s entered the search area at [%f,%f]", key, location.latitude, location.longitude));
-                if(poweredOn) turnOnAlert(true, true, true);
-            }
-
-            @Override
-            public void onKeyExited(String key) {
-                System.out.println(String.format("Key %s is no longer in the search area", key));
-                if(poweredOn) turnOffAlert();
-            }
-
-            @Override
-            public void onKeyMoved(String key, GeoLocation location) {
-                System.out.println(String.format("Key %s moved within the search area to [%f,%f]", key, location.latitude, location.longitude));
-            }
-
-            @Override
-            public void onGeoQueryReady() {
-                System.out.println("All initial data has been loaded and events have been fired!");
-            }
-
-            @Override
-            public void onGeoQueryError(DatabaseError error) {
-                System.err.println("There was an error with this query: " + error);
-            }
-        });
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -155,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements Alert.OnFragmentI
                     poweredOn = false;
                 }
                 else { // turn on
-                    turnOnAlert(true, true, true);
+                    //turnOnAlert(true, true, true);
 //                    getSupportFragmentManager().beginTransaction().setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out).show(nFrag).commit();
                     item.setIcon(R.drawable.ic_on);
                     item.setTitle("TURN OFF");
@@ -230,27 +158,7 @@ public class MainActivity extends AppCompatActivity implements Alert.OnFragmentI
     }
 
 
-    private void signInAnonymously() {
-        Log.d("Login Anon", "Start Anon Login");
-        // [START signin_anonymously]
-        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(Task<AuthResult> task) {
-                Log.d("Login Anon", "signInAnonymously:onComplete:" + task.isSuccessful());
-
-                // If sign in fails, display a message to the user. If sign in succeeds
-                // the auth state listener will be notified and logic to handle the
-                // signed in user can be handled in the listener.
-                if (!task.isSuccessful()) {
-                    Log.w("Login Anon", "signInAnonymously", task.getException());
-
-                }
-            }
 
 
-        });
 
-
-        // [END signin_anonymously]
-    }
 }
