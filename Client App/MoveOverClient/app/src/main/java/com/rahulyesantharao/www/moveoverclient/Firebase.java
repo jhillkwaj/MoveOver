898eaf0937eddcbb4c6fe3956356eebdf8ee27dc
Justin Hill <jhillkwaj@gmail.com>
2016-08-07 02:55:02 -0500

    Types of emergency added to client

--- Client App/MoveOverClient/app/src/main/java/com/rahulyesantharao/www/moveoverclient/Firebase.java
+++ Client App/MoveOverClient/app/src/main/java/com/rahulyesantharao/www/moveoverclient/Firebase.java
@@ -15,9 +15,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by justi on 8/6/2016.
@@ -103,7 +105,41 @@ public class Firebase {
                     @Override
                     public void onKeyEntered(String key, GeoLocation location) {
                         System.out.println(String.format("Key %s entered the search area at [%f,%f]", key, location.latitude, location.longitude));
-                        if (mainActivity.poweredOn) mainActivity.turnOnAlert(true, true, true);
+                        if (mainActivity.poweredOn){
+                            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("geo-loc/"+key);
+
+                             ref.child("type").addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot snapshot) {
+                                    //Log.d("test",""+snapshot.getValue());
+                                    if(snapshot.getValue().equals("Fire"))
+                                    {
+                                        Log.d("test","Fire");
+                                        mainActivity.turnOnAlert(false,false,true);
+                                    }
+                                    else if(snapshot.getValue().equals("Ambulance"))
+                                    {
+                                        Log.d("test","Ambulance");
+                                        mainActivity.turnOnAlert(false,true,false);
+                                    }
+                                    else
+                                    {
+                                        Log.d("test","Police");
+                                        mainActivity.turnOnAlert(true,false,false);
+                                    }
+                                }
+
+                                 @Override
+                                 public void onCancelled(DatabaseError databaseError) {
+                                     Log.e("Error",databaseError.toString());
+                                     mainActivity.turnOnAlert(true,true,true);
+                                 }
+
+
+                            });
+
+
+                        }
                     }
 
                     @Override
